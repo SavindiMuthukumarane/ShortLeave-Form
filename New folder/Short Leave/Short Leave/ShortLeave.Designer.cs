90a5242f613ab23e5d1b0d2e558f9a18064a4dab
SavindiMuthukumarane <savindimuthukumarane@gmail.com>
2025-06-10 15:32:00 +0530

    Add files via upload

---
+++ New folder/Short Leave/Short Leave/ShortLeave.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Short_Leave
+{
+    partial class ShortLeave
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtRoomNumber = new System.Windows.Forms.TextBox();
+            this.txtReason = new System.Windows.Forms.TextBox();
+            this.txtLeaveDate = new System.Windows.Forms.DateTimePicker();
+            this.parentmail = new System.Windows.Forms.Label();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(79, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Reason";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Room Number";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(79, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Short Leave Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(79, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Student ID";
+            // 
+            // Submit
+            // 
+            this.Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Submit.Location = new System.Drawing.Point(213, 371);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(92, 33);
+            this.Submit.TabIndex = 5;
+            this.Submit.Text = "Submit";
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            this.Submit.MouseEnter += new System.EventHandler(this.Submit_MouseEnter);
+            this.Submit.MouseLeave += new System.EventHandler(this.Submit_MouseLeave);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(354, 371);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 33);
+            this.clear.TabIndex = 6;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            this.clear.MouseEnter += new System.EventHandler(this.clear_MouseEnter);
+            this.clear.MouseLeave += new System.EventHandler(this.clear_MouseLeave);
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(253, 127);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(260, 22);
+            this.txtStudentName.TabIndex = 7;
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(253, 172);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(260, 22);
+            this.txtStudentID.TabIndex = 8;
+            this.txtStudentID.TextChanged += new System.EventHandler(this.txtStudentID_TextChanged);
+            // 
+            // txtRoomNumber
+            // 
+            this.txtRoomNumber.Location = new System.Drawing.Point(253, 216);
+            this.txtRoomNumber.Name = "txtRoomNumber";
+            this.txtRoomNumber.Size = new System.Drawing.Size(260, 22);
+            this.txtRoomNumber.TabIndex = 9;
+            // 
+            // txtReason
+            // 
+            this.txtReason.Location = new System.Drawing.Point(253, 287);
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(260, 22);
+            this.txtReason.TabIndex = 11;
+            // 
+            // txtLeaveDate
+            // 
+            this.txtLeaveDate.Location = new System.Drawing.Point(253, 254);
+            this.txtLeaveDate.Name = "txtLeaveDate";
+            this.txtLeaveDate.Size = new System.Drawing.Size(260, 22);
+            this.txtLeaveDate.TabIndex = 13;
+            // 
+            // parentmail
+            // 
+            this.parentmail.AutoSize = true;
+            this.parentmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parentmail.Location = new System.Drawing.Point(79, 289);
+            this.parentmail.Name = "parentmail";
+            this.parentmail.Size = new System.Drawing.Size(0, 20);
+            this.parentmail.TabIndex = 14;
+            // 
+            // txtmail
+            // 
+            this.txtmail.Location = new System.Drawing.Point(253, 328);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(219, 22);
+            this.txtmail.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(79, 328);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 22);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Parent Mail";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(469, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "@gmail.com";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Navy;
+            this.label8.Location = new System.Drawing.Point(195, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(376, 32);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Short Leave Request Form";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Short_Leave.Properties.Resources.nsbm_logo2;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label9.Location = new System.Drawing.Point(0, 427);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(800, 23);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "© NSBM Green University Hostel Complex";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // ShortLeave
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.parentmail);
+            this.Controls.Add(this.txtLeaveDate);
+            this.Controls.Add(this.txtReason);
+            this.Controls.Add(this.txtRoomNumber);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ShortLeave";
+            this.Text = "Submit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.TextBox txtRoomNumber;
+        private System.Windows.Forms.TextBox txtReason;
+        private System.Windows.Forms.DateTimePicker txtLeaveDate;
+        private System.Windows.Forms.Label parentmail;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ New folder/Short Leave/Short Leave/ShortLeave.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Mail;
+using System.Diagnostics;
+
+
+namespace Short_Leave
+{
+    
+    public partial class ShortLeave: Form
+    {
+        string connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\savin\OneDrive - NSBM\Attachments\New folder\Database11.accdb";
+
+        public ShortLeave()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            //clear all inputs
+            txtStudentName.Text = "";
+            txtStudentID.Text = "";
+            txtRoomNumber.Text = "";
+            txtLeaveDate.Text = "";
+            txtReason.Text = "";
+            txtmail.Text = "";
+
+           
+    
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+
+            
+            // Get values from input fields
+            string studentName = txtStudentName.Text;
+            string studentID = txtStudentID.Text;
+            string roomNumber = txtRoomNumber.Text;
+            string leaveDate = txtLeaveDate.Text;
+            string reason = txtReason.Text;
+            string parentmail = txtmail.Text.Trim();
+            
+            //prevent double @gmail.com if user typed it
+            if (parentmail.EndsWith("@gmail.com"))
+                parentmail = parentmail.Substring(0, parentmail.Length - 10);
+            //Add @gmail.com
+            parentmail += "@gmail.com";
+
+            // Check if any field is empty
+            if (string.IsNullOrWhiteSpace(studentName) ||
+                string.IsNullOrWhiteSpace(studentID) ||
+                string.IsNullOrWhiteSpace(roomNumber) ||
+                string.IsNullOrWhiteSpace(leaveDate) ||
+                string.IsNullOrWhiteSpace(reason)||
+                string.IsNullOrWhiteSpace(parentmail))
+            {
+                MessageBox.Show("Please fill in all required fields.",
+                                "Missing Information",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                return;
+            }
+            //Email Format Validation for Gamil address
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@gmail\.com$";
+            if(!Regex.IsMatch(parentmail,emailPattern))
+            {
+                MessageBox.Show("Please enter a valid Gmail username(e.g.,jonh.doe).Do not type @gmail.com-it is added automatically.", "Invalid Email",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Insert in to database
+            using (OleDbConnection conn = new OleDbConnection(connString))
+            {
+                string query = "INSERT INTO [Short Leave] ([Student Name],[Student ID],[Room Number],[Short Leave Date],[Reason]) VALUES (?, ?, ?,?,?)";
+
+                using (OleDbCommand cmd = new OleDbCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("?", studentName);
+                    cmd.Parameters.AddWithValue("?", studentID);
+                    cmd.Parameters.AddWithValue("?", roomNumber);
+                    cmd.Parameters.AddWithValue("?", leaveDate);
+                    cmd.Parameters.AddWithValue("?", reason);
+
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Leave request submitted successfully!");
+                    
+                    
+        
+                        //compose email
+
+                        string Subject = $"Short Leave Notification for {studentName}(ID:{studentID})";
+                        string Body = $" Dear Parent,\n\nThis is to inform you that your child {studentName} (ID:{studentID}) has requested a " +
+                           $" short leave on {leaveDate} for the following reason:\n\n{reason}\n\n .Thank You for your cooperation.";
+                        
+                        //encode for URL
+
+                       string encodedSubject =Uri.EscapeDataString(Subject);
+                       string encodedBody = Uri.EscapeDataString(Body);
+
+                        string mailtoLink = $"https://mail.google.com/mail/?view=cm&fs=1&to={parentmail}&su={Uri.EscapeDataString(Subject)}&body={Body}";
+
+
+                        //open Gmail compose window
+                        Process.Start(new ProcessStartInfo(mailtoLink)
+                        { UseShellExecute = true });
+                    }
+                    
+
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Leave saved,but failed to open mail " + ex.Message);
+                    }
+                }
+            }
+
+            
+
+
+        }
+
+        private void Submit_MouseEnter(object sender, EventArgs e)
+        {
+            Submit.BackColor = ColorTranslator.FromHtml("#66CC00");
+
+        }
+
+        private void Submit_MouseLeave(object sender, EventArgs e)
+        {
+            Submit.BackColor = SystemColors.Control;
+
+        }
+
+        private void clear_MouseEnter(object sender, EventArgs e)
+        {
+           clear.BackColor = ColorTranslator.FromHtml("#C0C0C0");
+
+        }
+
+        private void clear_MouseLeave(object sender, EventArgs e)
+        {
+            clear.BackColor = SystemColors.Control;
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtStudentID_TextChanged(object sender, EventArgs e)
+        {
+            if (txtStudentID.Text.Length > 6)
+            {
+                MessageBox.Show("Student ID should not be longer than 6 digits.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtStudentID.Text = txtStudentID.Text.Substring(0, 6);  // Trim to 6 digits
+                txtStudentID.SelectionStart = txtStudentID.Text.Length;  // Move cursor to the end
+            }
+        }
+    }
+}
+    
+
